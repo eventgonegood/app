5165588672dbb9a6c153fa351787483b4de81329
Dru Sellers <dru@drusellers.com>
2016-01-31 10:52:23 -0600

    Formatting and clean up

--- src/app/scheduling/endpoint.clj
+++ src/app/scheduling/endpoint.clj
@@ -15,72 +15,30 @@
                :friday {:label "Friday"}
                :saturday {:label "Saturday"}})
 
-(def karhu-schedule {:dow-layout [:sunday :monday :tuesday :wednesday :thursday :saturday]
-                     :classes {:monday [{:class "Fitness" :times [{:title "5:30am" :start "05:30:00" :end "06:30:00"}
-                                                                  {:title "6:30am" :start "06:30:00" :end "07:30:00"}
-                                                                  {:title "4:30pm" :start "16:30:00" :end "17:30:00"}
-                                                                  {:title "5:30pm" :start "17:30:00" :end "18:30:00"}
-                                                                  {:title "6:30pm" :start "18:30:00" :end "19:30:00"}
-                                                                  {:title "7:30pm" :start "19:30:00" :end "20:30:00"}]}
-                                        {:class "Sport" :times [{:title "5:30am" :start "05:30:00" :end "06:30:00"}
-                                                                {:title "6:30am" :start "06:30:00" :end "07:30:00"}
-                                                                {:title "Noon" :start "12:00:00" :end "13:00:00"}]}
-                                        {:class "Competition" :times [{:title "5:30am" :start "05:30:00" :end "07:00:00"}
-                                                                      {:title "10am" :start "10:00:00" :end "11:30:00"}
-                                                                      {:title "6pm" :start "18:00:00" :end "19:30:00"}]}
-                                        {:class "Weightlifting" :times [{:title "6:30am" :start "06:30:00" :end "07:30:00"}
-                                                                        {:title "5:30pm" :start "17:30:00" :end "18:30:00"}
-                                                                        {:title "7:00pm" :start "19:30:00" :end "20:00:00"}]}]
-
-                               :tuesday [{:class "Fitness" :times [{:title "5:30am" :start "05:30:00" :end "06:30:00"}
-                                                                   {:title "6:30am" :start "06:30:00" :end "07:30:00"}
-                                                                   {:title "4:30pm" :start "16:30:00" :end "17:30:00"}
-                                                                   {:title "5:30pm" :start "17:30:00" :end "18:30:00"}
-                                                                   {:title "6:30pm" :start "18:30:00" :end "19:30:00"}
-                                                                   {:title "7:30pm" :start "19:30:00" :end "20:30:00"}]}
-                                         {:class "Sport" :times [{:title "5:30am" :start "05:30:00" :end "06:30:00"}
-                                                                 {:title "6:30am" :start "06:30:00" :end "07:30:00"}
-                                                                 {:title "Noon" :start "12:00:00" :end "13:00:00"}]}
-                                         {:class "Competition" :times [{:title "5:30am" :start "05:30:00" :end "07:00:00"}
-                                                                       {:title "10am" :start "10:00:00" :end "11:30:00"}
-                                                                       {:title "6pm" :start "18:00:00" :end "19:30:00"}]}
-                                         {:class "Weightlifting" :times [{:title "6:30am" :start "06:30:00" :end "07:30:00"}
-                                                                         {:title "5:30pm" :start "17:30:00" :end "18:30:00"}
-                                                                         {:title "7:00pm" :start "19:30:00" :end "20:00:00"}]}]
-                               :wednesday []
-                               :thursday []
-                               :friday []
-                               :saturday []
-                               :sunday []}})
-
 (defn extract-resources
   "convert a schedule row to a cal resource"
   [data]
   (let [classes (distinct (map (fn [r] (:class r)) (:classes data)))]
-  (map (fn [dow] {:id dow
-                  :title (get-in the-week [dow :label])
-                  :children (map (fn [class]
-                                   {:id (str dow "-" class)
-                                    :title class}) classes)}) (:dow-layout data))
-    )
-  )
+    (map (fn [dow] {:id dow
+                    :title (get-in the-week [dow :label])
+                    :children (map (fn [class]
+                                     {:id (str dow "-" class)
+                                      :title class}) classes)}) (:dow-layout data))))
 
 (defn format-time [date]
   (.format (java.text.SimpleDateFormat. "HH:mm:ss") date))
 
 (defn extract-events [data]
   ;day_of_week, class, title, start_at, end_at, duration
-  (map (fn [row]{:id (str (:day_of_week row) "-" (:class row) "-" (:title row))
+  (map (fn [row] {:id (str (:day_of_week row) "-" (:class row) "-" (:title row))
                   :resourceId (str (:day_of_week row) "-" (:class row))
-                :start (str "2015-08-07T" (format-time (:start_at row)))
-                :end (str "2015-08-07T" (format-time (:end_at row)))
-                :title (:title row)
-                 } ) (:classes data)))
+                  :start (str "2015-08-07T" (format-time (:start_at row)))
+                  :end (str "2015-08-07T" (format-time (:end_at row)))
+                  :title (:title row)}) (:classes data)))
 
 (defn convert-to-fullcal [data]
   {:resources (extract-resources data)
-   :events (extract-events data)
-   })
+   :events (extract-events data)})
 
 (defn add-slot [dow]
   [:a {:href (str "/scheduling/add/" dow)} [:i {:class "fa fa-plus"}]])
--- src/app/scheduling/schedule.clj
+++ src/app/scheduling/schedule.clj
@@ -7,24 +7,19 @@
 
 (defn convert [row]
   (as-> row $
-    (assoc $ :day_of_week (keyword (.getValue (:day_of_week $))))
-    )
-  )
+        (assoc $ :day_of_week (keyword (.getValue (:day_of_week $))))))
 (def days-of-week [:monday :tuesday :wednesday :thursday :friday :saturday :sunday])
 
 (defn weekly [db organization_id]
-  (let [raw-values (sql-gym-schedule {:organization_id organization_id} {
-                                                                 :connection db
-                                                                 :row-fn convert
-                                                                 })
+  (let [raw-values (sql-gym-schedule {:organization_id organization_id} {:connection db
+                                                                         :row-fn convert})
         ;day_of_week, class, title, start_at, end_at, duration
         ;group by day of week, class {:day_of_week
         ;classes (map #({(:class $) []}) raw-values)
         ;init-struct (map (fn [dow] {dow (mapv (fn [c] {:class c :times []}) classes)}) days-of-week)
         ; {:monday [{:class "name" :times []},{}]}
         ;processed (map (fn [row] ) raw-values)
-        ]
+]
 
-        {:dow-layout [:sunday :monday :tuesday :wednesday :thursday :saturday]
-         :classes raw-values }
-    ))
+    {:dow-layout [:sunday :monday :tuesday :wednesday :thursday :saturday]
+     :classes raw-values}))
